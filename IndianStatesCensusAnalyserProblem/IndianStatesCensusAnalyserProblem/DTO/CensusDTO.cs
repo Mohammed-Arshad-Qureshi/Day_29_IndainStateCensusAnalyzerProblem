d21c122dc32454034481979fb0a12cde28e4ff75
Arshad <mohammedarshad183@gmail.com>
2022-06-04 14:07:28 +0530

    Added : program file to print Welcome message

---
+++ IndianStatesCensusAnalyserProblem/IndianStatesCensusAnalyserProblem/DTO/CensusDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndianStatesCensusAnalyserProblem.DTO
+{
+   public class CensusDTO
+    {
+        public int serialNumber;
+        public string stateName;
+        public string state;
+        public int tin;
+        public string stateCode;
+        public int population;
+        public long area;
+        public long density;
+        public long housingUnits;
+        public double totalArea;
+        public double waterArea;
+        public double landArea;
+        public double populationDensity;
+        public double housingDensity;
+
+        public CensusDTO(StateCodeDAO stateCodeDao)
+        {
+            this.serialNumber = stateCodeDao.serialNumber;
+            this.stateName = stateCodeDao.stateName;
+            this.tin = stateCodeDao.tin;
+            this.stateCode = stateCodeDao.stateCode;
+        }
+        public CensusDTO()
+    }
+}
